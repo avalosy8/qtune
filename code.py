aad9656a0949c7adb1914e7d956536dc292e1dcb
Yenessa Avalos-Maldonado <yenessa@Yenessas-MacBook-Pro.local>
2022-11-17 15:45:41 -0500

    turning two motors

--- code.py
+++ code.py
@@ -1,53 +1,42 @@
+# SPDX-FileCopyrightText: 2021 jedgarpark for Adafruit Industries
+# SPDX-License-Identifier: MIT
+# Pico servo demo
+# Hardware setup:
+# Servo on GP0 with external 5V power supply
+# Button on GP3 and ground
 import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
 import pwmio
 from adafruit_motor import servo
-
 print("Servo test")
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+led.value = True
 
 # Servo setup
-pwm_servo = pwmio.PWMOut(board.GP0, duty_cycle=2 ** 15, frequency=50)
+pwm_servo = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 servo1 = servo.ContinuousServo(
-    pwm_servo, min_pulse=500, max_pulse=2200
-)  # tune pulse for specific servo
+    pwm_servo, min_pulse=750, max_pulse=2250
+)
 
-# Servo test
-def servo_direct_test():
-    print("servo test: 90")
-    servo1.angle = 90
-    time.sleep(2)
-    print("servo test: 0")
-    servo1.angle = 0
-    time.sleep(2)
-    print("servo test: 90")
-    servo1.angle = 90
-    time.sleep(2)
-    print("servo test: 180")
-    servo1.angle = 180
-    time.sleep(2)
-
-
-# Servo smooth test
-def servo_smooth_test():
-    print("servo smooth test: 180 - 0, -1º steps")
-    for angle in range(180, 0, -1):  # 180 - 0 degrees, -1º at a time.
-        servo1.angle = angle
-        time.sleep(0.01)
-    time.sleep(1)
-    print("servo smooth test: 0 - 180, 1º steps")
-    for angle in range(0, 180, 1):  # 0 - 180 degrees, 1º at a time.
-        servo1.angle = angle
-        time.sleep(0.01)
-    time.sleep(1)
+# takes in measured frequency and target frequency, moves servo accordingly
+def tune(freq, target_freq, servo):
+    if freq > target_freq:
+        move_servo(1,1,servo)
+    elif freq < target_freq:
+        move_servo(-1,1,servo)
+        
+def move_servo(throttle, interval, servo):
+    servo.throttle = throttle
+    time.sleep = interval
 
 while True:
-    # testing different movements
-    # servo_direct_test()
-    # servo_smooth_test() 
-
-    # moving servo left and right
-    servo1.throttle = 1
+    servo1.throttle = 0.1
     time.sleep(1)
-    servo1.throttle = -1
+    servo1.throttle = 0.0
+    time.sleep(1)
+    servo1.throttle = -0.1
+    time.sleep(1)
+    servo1.throttle = 0.0
     time.sleep(1)
