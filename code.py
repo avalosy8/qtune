359a9082fc49c903d85ca3b029ca9d0345cec389
Yenessa Avalos-Maldonado <yenessa@Yenessas-MacBook-Pro.local>
2022-11-17 15:56:37 -0500

    actual new code

--- code.py
+++ code.py
@@ -6,37 +6,64 @@
 # Button on GP3 and ground
 import time
 import board
-from digitalio import DigitalInOut, Direction, Pull
+import digitalio
 import pwmio
 from adafruit_motor import servo
-print("Servo test")
-led = DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
+
+string_notes = {'E': 82, 'A': 110, 'D': 147, 'G': 196, 'B': 247, 'E': 330 }
+
+# LED setup
+LED_PIN = board.D13
+led = digitalio.DigitalInOut(LED_PIN)
+led.switch_to_output(value=False)
 led.value = True
 
 # Servo setup
-pwm_servo = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+pwm_servo1 = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
 servo1 = servo.ContinuousServo(
-    pwm_servo, min_pulse=750, max_pulse=2250
+    pwm_servo1, min_pulse=500, max_pulse=2360
+)
+pwm_servo2 = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+servo2 = servo.ContinuousServo(
+    pwm_servo2, min_pulse=600, max_pulse=2360
 )
 
 # takes in measured frequency and target frequency, moves servo accordingly
-def tune(freq, target_freq, servo):
+def tune(freq, target_freq):
     if freq > target_freq:
-        move_servo(1,1,servo)
+        move_servo(0.1,0.1)
+        freq -= 5 # measure freq here
+        print("Frequency too high , strum again!")
     elif freq < target_freq:
-        move_servo(-1,1,servo)
+        move_servo(-0.1,0.1)
+        freq += 5 # measure freq here
+        print("Frequency too low, strum again!")
+    print("Tuning...\n input_freq=",freq)
+    return freq
         
-def move_servo(throttle, interval, servo):
-    servo.throttle = throttle
-    time.sleep = interval
+def move_servo(throttle, interval):
+    servo1.throttle = throttle
+    servo2.throttle = throttle
+    time.sleep(interval)
+    servo1.throttle = 0
+    servo2.throttle = 0
+    time.sleep(interval)
 
-while True:
-    servo1.throttle = 0.1
-    time.sleep(1)
-    servo1.throttle = 0.0
-    time.sleep(1)
-    servo1.throttle = -0.1
-    time.sleep(1)
-    servo1.throttle = 0.0
-    time.sleep(1)
+note = input("Select string: ").upper()
+
+output_freq = 600
+target_freq = string_notes[note]
+print("Target frequency: ", target_freq, "\n")
+
+# Test with test output frequency
+while output_freq != target_freq:
+    output_freq = tune(output_freq, target_freq)
+led.value = False
+time.sleep(2)
+output_freq = 300
+led.value = True
+
+# Test with different output freq
+while output_freq != target_freq:
+    output_freq = tune(output_freq, target_freq)
+led.value = False
