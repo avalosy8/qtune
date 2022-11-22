7ed4ec7d0fc2380ef8b14b7362fea77db5045115
Yenessa Avalos-Maldonado <yenessa@Yenessas-MacBook-Pro.local>
2022-11-22 12:34:27 -0500

    added LCD functionality

--- code.py
+++ code.py
@@ -9,9 +9,10 @@ import board
 import digitalio
 import pwmio
 from adafruit_motor import servo
+import adafruit_character_lcd.character_lcd as characterlcd
 
 string_notes = {'E': 82, 'A': 110, 'D': 147, 'G': 196, 'B': 247, 'E': 330 }
-
+notes = {0: 'E', 1: 'A', 2: 'D', 3: 'G', 4: 'B', 5: 'E'} 
 # LED setup
 LED_PIN = board.D13
 led = digitalio.DigitalInOut(LED_PIN)
@@ -28,42 +29,91 @@ servo2 = servo.ContinuousServo(
     pwm_servo2, min_pulse=600, max_pulse=2360
 )
 
+# Pin Config:
+lcd_rs = digitalio.DigitalInOut(board.D5)
+lcd_en = digitalio.DigitalInOut(board.D6)
+lcd_d7 = digitalio.DigitalInOut(board.D10)
+lcd_d6 = digitalio.DigitalInOut(board.D11)
+lcd_d5 = digitalio.DigitalInOut(board.D9)
+lcd_d4 = digitalio.DigitalInOut(board.D12)
+lcd_backlight = digitalio.DigitalInOut(board.D4)
+lcd_columns = 16
+lcd_rows = 2
+
+# Buttons config
+button1 = digitalio.DigitalInOut(board.D25)
+button2 = digitalio.DigitalInOut(board.D24)
+button1.direction = digitalio.Direction.INPUT
+button1.pull = digitalio.Pull.UP # changes string
+button2.direction = digitalio.Direction.INPUT
+button2.pull = digitalio.Pull.UP # selects string
+
+# Initialize the lcd class
+lcd = characterlcd.Character_LCD_Mono(
+    lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight
+)
+
 # takes in measured frequency and target frequency, moves servo accordingly
 def tune(freq, target_freq):
     if freq > target_freq:
-        move_servo(0.1,0.1)
+        move_servo(1,0.1)
         freq -= 5 # measure freq here
+        lcd.clear()
         print("Frequency too high , strum again!")
+        lcd.message = str(freq) + " too high,\nstrum again!"
     elif freq < target_freq:
-        move_servo(-0.1,0.1)
+        move_servo(-1,0.1)
         freq += 5 # measure freq here
+        lcd.clear()
         print("Frequency too low, strum again!")
+        lcd.message = str(freq) + " too low,\nstrum again!"
     print("Tuning...\n input_freq=",freq)
     return freq
         
 def move_servo(throttle, interval):
     servo1.throttle = throttle
+    time.sleep(interval)
     servo2.throttle = throttle
     time.sleep(interval)
     servo1.throttle = 0
+    time.sleep(interval)
     servo2.throttle = 0
     time.sleep(interval)
 
-note = input("Select string: ").upper()
+lcd.message = "Select string"
+# note = input("Select string: ").upper()
+selected = False
+idx = 0
+note = notes[idx] 
 
-output_freq = 600
+while not selected: 
+    if not button2.value:
+        # select curr string
+        selected = True
+    if not button1.value:
+        if idx >= 5:
+            idx = 0
+        else:
+            idx += 1
+        time.sleep(0.5)  # debounce delay 
+        note = notes[idx]
+        print(note)
+    lcd.message = "Select string "+note
+print("Done selecting string")
+lcd.clear()
+lcd.message = "Selected string \n" + note.upper()
+time.sleep(0.5)
+
+led.value = True
+output_freq = 50
 target_freq = string_notes[note]
 print("Target frequency: ", target_freq, "\n")
+lcd.clear()
+lcd.message = "Target: " + str(target_freq)
 
 # Test with test output frequency
 while output_freq != target_freq:
     output_freq = tune(output_freq, target_freq)
-led.value = False
-time.sleep(2)
-output_freq = 300
-led.value = True
-
-# Test with different output freq
-while output_freq != target_freq:
-    output_freq = tune(output_freq, target_freq)
+lcd.clear()
+lcd.message = "Finished\ntunning!"
 led.value = False
